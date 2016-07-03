64d65ec6f79ec31e2a7bc9a59783907770c293ba
Mauricio Eduardo <maubot01@gmail.com>
2016-07-03 15:17:30 -0430

    Hello World
    Primer programa hecho en C
    First program made in C

---
+++ 1)Fundamentos de un programa en lenguaje C/holamundo-helloworld.c
@@ -0,0 +1,15 @@
+//Incluimos la librería principal que contiene la funciones principales
+//We include the main library containing the main functions
+#include <stdio.h>
+
+//Función principal de un programa en C, donde se llamaran a las demás funciones
+// Main function of a C program where to call other functions
+int main (){
+
+	//Función para imprimir por pantalla que muestra una cadena de caracteres entre las ""
+	//Function to print the screen showing a character string between ""
+	printf("Hola Mundo, Hello World\n"); //El \n hace un salto de línea
+										//The \n makes a line break
+
+	return 0;
+}
