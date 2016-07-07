761fc9740819b9cebdc9fb7ca88f29149e788ff3
Mauricio Eduardo <maubot01@gmail.com>
2016-07-07 10:04:28 -0430

    Sucesión Fibonacci

---
+++ 10)Recursividad Recursion/Sucesión Fibonacci/Succeción Fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+int fibonacci (int n){
+	if (n<2)
+		return n;
+	else 
+		return fibonacci(n-1)+ fibonacci(n-2);
+}
+
+int main (){
+	int num=0, res=0;
+
+	printf("NUMEROS DE FIBONACCI\n" );
+	printf("Introduce el numero de numeros\n" );
+	scanf("%i",&num);
+	printf("t");
+
+	for(int i=0;i<=num-1;i++){
+		res=fibonacci(i);
+		printf("%i ",res);
+	}
+	printf("n");
+	getch();
+	return 0;
+}
