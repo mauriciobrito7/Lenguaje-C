8d593f439f03eb58aa9f8f9500effc24765b1eb6
Mauricio Eduardo <maubot01@gmail.com>
2016-07-13 14:43:38 -0430

    Máximo común divisor

--- 10)Recursividad Recursion/Maximo comun divisor/mcd.c
+++ 10)Recursividad Recursion/Maximo comun divisor/mcd.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <conio.h>
 
 //Algoritmo de Euclides
 //mcd(15,9)=mcd(9,15%9)=mcd(9,6)=mcd(6,3)=mcd(3,0), mcd=3
@@ -11,29 +10,28 @@ int recursion(int a, int b){
 }
 
 int main (){
-int a,b;
+	int a,b;
 
-printf("Recursion\n");
-do{
-	printf("Login the first number:\n");
-	scanf("%i",&a);
+	printf("Recursion\n");
+	do{
+		printf("Login the first number:\n");
+		scanf("%i",&a);
 
-}while(a<0);
+	}while(a<0);
 
-do{
-	printf("Login the second number:\n");
-	scanf("%i",&b);
+	do{
+		printf("Login the second number:\n");
+		scanf("%i",&b);
 
-}while(a<0);
+	}while(a<0);
 
-if(a>b){
-	printf("The mcd=%i\n",recursion(a,b));
-}
-else{
-	printf("The mcd=%i\n",recursion(a,b));	
-}
-
-getch();
-return 0;
+	if(a>b){
+		printf("The mcd=%i\n",recursion(a,b));
+	}
+	else{
+		printf("The mcd=%i\n",recursion(a,b));	
+	}
+	system("pause");
+	return 0;
 
 }
