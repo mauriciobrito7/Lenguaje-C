dfcde40a683cf03b302d4eb9361070939d8d9f45
Mauricio Eduardo <maubot01@gmail.com>
2016-07-13 14:47:31 -0430

    Número mayor en un vector

--- 10)Recursividad Recursion/Número mayor de un vector/NumeroMayor.c
+++ 10)Recursividad Recursion/Número mayor de un vector/NumeroMayor.c
@@ -1,7 +1,7 @@
 //Hacer una funcion recursiva que diga cual el numero mayor dentro de un vector
 //Make a recursive function that says that the largest number in a vector
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 #define L 10
 
 int mayor(int v[], int maximo, int x){
@@ -15,8 +15,7 @@ int mayor(int v[], int maximo, int x){
 int main (){
 
 	int v[L]={2,3,6,8,12,20,50,22,4,5};
-
 	int maximo=v[0];
 	printf("El numero maximo es :%i", mayor(v,maximo,L));
-	getch();
+	system("pause");
 }
