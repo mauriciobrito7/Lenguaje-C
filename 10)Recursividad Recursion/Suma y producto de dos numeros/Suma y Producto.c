d00e37a8723ca9d94c6ea23c6b7b523bb8fe59b2
Mauricio Eduardo <maubot01@gmail.com>
2016-07-13 14:51:51 -0430

    Suma y Producto

--- 10)Recursividad Recursion/Suma y producto de dos numeros/Suma y Producto.c
+++ 10)Recursividad Recursion/Suma y producto de dos numeros/Suma y Producto.c
@@ -1,11 +1,10 @@
 //Hacer la suma y el producto de dos numeros de manera recursiva
 #include <stdio.h>
-#include <conio.h>
+#include <stdlib.h>
 
 int suma(int n, int n2){
 	if(n2==0)
 		return n;
-
 	return suma(++n,--n2);
 }
 
@@ -13,14 +12,12 @@ int multiplicacion (int n, int n2){
 	if (n2==1)
 		return n;
 	return n + multiplicacion(n, --n2);
-
 }
-
 
 int main (){
 	int  n=8, n2=6;
 	printf("El resultado de la suma %i\n",suma(n,n2));
 	printf("El resultado de la multiplicacion %i\n",multiplicacion(n,n2));
-	getch();
+	system("pause");
 	return 0;
 }
