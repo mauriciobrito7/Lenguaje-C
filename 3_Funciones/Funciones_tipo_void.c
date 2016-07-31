3cbaaede5051ff7043880114bbdacfd858beff55
Mauricio Eduardo <maubot01@gmail.com>
2016-07-31 11:06:25 -0430

    Funciones de tipo void
    Funciones que no retornan ningun valor

---
+++ 3_Funciones/Funciones_tipo_void.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Funcion que no retorna nada
+void holaMundo(){
+	printf("Hola Mundo\n");
+}
+
+int main (){
+	holaMundo();
+	system("pause");
+	return 0;
+}
