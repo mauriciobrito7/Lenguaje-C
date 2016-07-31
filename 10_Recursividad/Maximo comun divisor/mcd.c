47881f15bf9a4b09091fc2f8cc8f9c9e2ab05877
Mauricio Eduardo <maubot01@gmail.com>
2016-07-31 08:37:27 -0430

    Maximo comun divisor

--- 10_Recursividad/Maximo comun divisor/mcd.c
+++ 10_Recursividad/Maximo comun divisor/mcd.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+//Maximo comun divisor
 //Algoritmo de Euclides
 //mcd(15,9)=mcd(9,15%9)=mcd(9,6)=mcd(6,3)=mcd(3,0), mcd=3
 
@@ -14,22 +14,22 @@ int main (){
 
 	printf("Recursion\n");
 	do{
-		printf("Login the first number:\n");
+		printf("Introduzca el primer numero:\n");
 		scanf("%i",&a);
 
 	}while(a<0);
 
 	do{
-		printf("Login the second number:\n");
+		printf("Introduzca el segundo numero:\n");
 		scanf("%i",&b);
 
 	}while(a<0);
 
 	if(a>b){
-		printf("The mcd=%i\n",recursion(a,b));
+		printf("El mcd=%i\n",recursion(a,b));
 	}
 	else{
-		printf("The mcd=%i\n",recursion(a,b));	
+		printf("El mcd=%i\n",recursion(a,b));	
 	}
 	system("pause");
 	return 0;
