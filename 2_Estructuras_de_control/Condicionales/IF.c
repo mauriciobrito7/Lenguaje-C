8411880ae990436e04b8c72a1d912edf074c947d
Mauricio Eduardo <maubot01@gmail.com>
2016-07-31 09:17:30 -0430

    IF

---
+++ 2_Estructuras_de_control/Condicionales/IF.c
@@ -0,0 +1,36 @@
+//Realiza un programa en C que lea tres números enteros positivos distintos e imprima el mayor 
+//y el menor valor de los tres. Intente que el programa realice el menor número de comparaciones
+//posibles entre los números.
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	int x,y,z,max,min;
+
+	printf("Ingrese el primer numero");
+	scanf("%i",&x);
+	printf("Ingrese el segundo numero");
+	scanf("%i",&y);
+	printf("Ingrese el tercer numero");
+	scanf("%i",&z);
+
+	if(x>y && x>z)
+		max=x;
+	else
+		max=y;
+	if(z>y)
+		max=z;
+	printf("El numero mayor es %i \n",max );
+
+	if(x<y && x<z)
+		min=x;
+	else
+		min=y;
+	if(z<y)
+		min=z;
+	printf("El numero menor es %i \n",min );
+	system("pause");
+	return 0;
+}
